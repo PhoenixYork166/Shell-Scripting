651d6bd766a9a6d9ca46311c46d71edb5b2cdecc
phoenixyork166 <berlinlee2017@gmail.com>
2023-10-11 15:54:21 +0800

    1

--- demo-03.sh
+++ demo-03.sh
@@ -26,18 +26,37 @@ UID_userName="Your UID is ${UID}\nYour username is: ${userName}";
 
 if [[ "${UID}" -eq "${rootID}" ]];
 then
-    printf "${UID_userName}\nYou're root";
-    exit 0;
+    printf "${UID_userName}\nYou're root\n";
+    #exit 0;
 elif [[ "${UID}" -ne "${ROOT}" ]];
 then
-    printf "${UID_userName}\nYou aren't root";
-    exit 1;
+    printf "${UID_userName}\nYou aren't root\n";
+    #exit 1;
 fi;
 
 # Display username
+#printf "\nYour username is: ${userName}";
 
 # Test if command succeeded
+# ${?} => Most recent executed command exit code
+if [[ "${?}" -ne 0 ]];
+then
+    printf "\nThe $(id -un) command did NOT execute successfully\n";
+    #exit 1;
+fi;
+
+printf "\nYour usename is: ${userName}";
 
 # You can use a string test conditional
+userNameToTestFor='phoenix';
+# = exact match
+# == pattern match
+if [[ "${userName}" = "${userNameToTestFor}" ]];
+then
+    printf "\nYour username matches ${userNameToTestFor}\n";
+elif [[ "${userName}" != "${userNameToTestFor}" ]];
+then
+    printf "\nYour username does NOT match ${userNameToTestFor}\n";
+fi;
 
 # Test for != for string
