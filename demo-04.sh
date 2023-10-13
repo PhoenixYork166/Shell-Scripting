5c30d4fda456567bd81f795fe88558cd56a3a9ec
root <phoenix166>
2023-10-13 04:39:55 -0400

    demo-04

--- demo-04.sh
+++ demo-04.sh
@@ -8,19 +8,22 @@
 read -p "Enter new user's username: " userName;
 #printf "${userName}";
 
-# Ask for real name 
-read -p "Enter new user's real name: " realName;
+# Ask for real name or Ticket number in COMMENT
+read -p "Enter new user's real name: " COMMENT;
 
 # Ask for password
 read -p "Enter new user's password: " password;
 
 # Create user
-# Using useradd command
-# useradd [options] LOGIN
-# useradd -D
-# useradd -D [options]
-
+# Using adduser command
+# adduser [--uid id] [--firstuid id] [--comment comment] 
+# adduser [--allow-all-names] [--allow-bad-names] 
+# "${VAR}" => Allow spaced comments to be stored into a var
+useradd -c "${COMMENT}" -m ${userName};
 
 # Set password for the user
+# CANNOT set password using passwd....
+echo "${userName}:${password}" | sudo chpasswd;
 
 # Force user to change password on 1st logon
+passwd -e ${userName};
